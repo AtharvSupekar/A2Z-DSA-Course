9856cda9160609541f80fd0c308dbbc761c7dd69
Atharv Supekar <atharvsupekar11@gmail.com>
2025-08-05 22:50:30 +0530

    Medium (2)

--- Java/Arrays/Medium/Sort an array of 0s, 1s and 2s/Solution.java
+++ Java/Arrays/Medium/Sort an array of 0s, 1s and 2s/Solution.java
@@ -1,8 +1,11 @@
-import java.util.HashMap;
-import java.util.Map;
 import java.util.Scanner;
-/*
+/*  
+ * For the Counting Sort
  * TC = O(2N)
+ * SC = O(1)
+ * 
+ * For Dutch National Flag Algorithm
+ * TC = O(n) - Single Pass Algo
  * SC = O(1)
  */
 public class Solution {
@@ -13,54 +16,31 @@ public class Solution {
         }
         System.out.println();
     }
-    private static int[] longestSubarray(int[] arr, int k){
-        long sum = 0;
-        int maxLen = 0, end_of_longest = -1, start_of_longest = -1;
-        Map<Long , Integer> preSumMap = new HashMap<>();
-        preSumMap.put(0L, -1);
-
-        for (int i = 0; i<arr.length; i++){
-            sum += arr[i];
-            if (preSumMap.containsKey(sum - k)){
-                int currentLen = i - preSumMap.get(sum-k);
-                if (currentLen > maxLen){
-                    maxLen = currentLen;
-                    start_of_longest = preSumMap.get(sum - k) + 1;
-                    end_of_longest = i;
-                }
-            }
-            if (!preSumMap.containsKey(sum)){
-                preSumMap.put(sum, i);
+    // Dutch National Flag Algorithm (DNFA)
+    private static void swap(int[] arr, int index1, int index2) {
+    int temp = arr[index1];
+    arr[index1] = arr[index2];
+    arr[index2] = temp;
+}
+    private static void sortColors(int[] nums) {
+        int n = nums.length;
+        int low = 0, mid = 0, high = n - 1;
+        while (mid <= high) {
+            switch (nums[mid]) {
+                case 0:
+                    swap(nums, low, mid);
+                    low++;
+                    mid++;
+                    break;
+                case 1:
+                    mid++;
+                    break;
+                case 2:
+                    swap(nums, mid, high);
+                    high--;
+                    break;
             }
         }
-        if (start_of_longest != -1) {
-            int length = end_of_longest - start_of_longest + 1;
-            int[] subArray = new int[length];
-            System.arraycopy(arr, start_of_longest, subArray, 0, length);
-            return subArray;
-        }
-        else {
-            return new int[0];
-        }
-    }
-
-    private static int longestSubarray2P(int[] arr, int k){
-        int maxLen = 0;
-        int n = arr.length;
-        int left = 0, right = 0, sum = 0;
-
-        while (right < n){
-            sum += arr[right];
-            while (sum > k) {
-                sum -= arr[left];
-                left++;
-            }
-            if (sum == k) {
-                maxLen = Math.max(maxLen, right - left + 1);
-            }
-            right++;
-        }
-        return maxLen;
     }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
@@ -72,10 +52,8 @@ public class Solution {
             for (int i = 0; i < n; i++){
                 arr[i] = sc.nextInt();
             }
-            int k = sc.nextInt();
-            int[] result = longestSubarray(arr, k);
-            printArray(result);
-            System.out.println(longestSubarray2P(arr, k));
+            sortColors(arr);
+            printArray(arr);
         }
         sc.close();
     }
--- Python/Arrays/Medium/Sort an array of 0s, 1s and 2s/Solution.py
+++ Python/Arrays/Medium/Sort an array of 0s, 1s and 2s/Solution.py
@@ -1,44 +1,28 @@
 ''' 
+    For the Counting Sort
     TC = O(2N)
     SC = O(1)
-'''
 
+    For Dutch National Flag Algorithm
+    TC = O(n) - Single Pass Algo
+    SC = O(1)
+'''
 from typing import List
 
-def Longest_Subarray(arr: List[int], k: int) -> List[int]:
-    sum, maxLen = 0, 0
-    preSumMap = {0: -1}
-    start_of_longest, end_of_longest = -1, -1
-    for i in range(0, len(arr)):
-        sum += arr[i]
-        if (sum - k) in preSumMap:
-            curr_length = i - preSumMap[sum - k]
-            if curr_length > maxLen:
-                maxLen = curr_length
-                start_of_longest = preSumMap[sum - k] + 1
-                end_of_longest = i
-
-        if sum not in preSumMap:
-            preSumMap[sum] = i
-    if start_of_longest != -1:
-        return arr[start_of_longest: end_of_longest + 1]
-    else:
-        return []
-
-def Longest_Subarray_2P(arr: List[int], k: int) -> List[int]:
-    left, right = 0, 0
-    sum, maxLen = 0, 0
-    n = len(arr)
-    
-    while right < n:
-        sum += arr[right]
-        while sum > k:
-            sum -= arr[left]
-            left += 1
-        if sum == k:
-            maxLen = max(maxLen, right - left + 1)
-        right += 1
-    return maxLen
+def sortColors(nums: List[int]) -> None:
+    low, mid = 0, 0
+    high = len(nums) - 1
+    while mid <= high:
+        match nums[mid]:
+            case 0:
+                nums[low], nums[mid] = nums[mid], nums[low]
+                low += 1
+                mid += 1
+            case 1:
+                mid += 1
+            case 2:
+                nums[mid], nums[high] = nums[high], nums[mid]
+                high -= 1
     
 # Main execution block: Handles input/output for testing
 t = int(input()) # Read number of test cases
@@ -46,5 +30,5 @@ while t > 0:
     t -= 1
     # Read elements for arr
     arr = list(map(int, input().split()))
-    k = int(input())
-    print(Longest_Subarray_2P(arr,k))
+    sortColors(arr)
+    print(arr)
